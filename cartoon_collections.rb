8f4431539aab50308b28c59d679ac65c563c916d
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-11 16:27:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |word|
-    word.include?(cheese_types)
+    word.include?(cheese_types[0] || cheese_types[1] || cheese_types[2])
   end
 end
